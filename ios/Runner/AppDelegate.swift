4c78ae2969c3a714fcdd4bd508f527805ba97be2
quotto <5211139+quotto@users.noreply.github.com>
2022-11-04 00:55:35 +0900

    [test]ネイティブ（iOS）でのアラーム試行

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -24,15 +24,15 @@ import workmanager
             guard let args = call.arguments as? [String: NSNumber] else {return}
 
             let now = Date()
-            var nextDate = now.addingTimeInterval(args["duration"]!.toInt()
+            var nextDate = now.addingTimeInterval(args["duration"]!.toInt())
             var dateComponentsDay = Calendar.dateComponents(in: TimeZone.current, from:nextDate)
 
             let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponentsDay, repeats: false)
 
             let content = UNMutableNotificationContent()
-                   content.title = "AndroidLikeAlerm"
-                   content.body = args["content"]!
-                   content.sound = UNNotificationSound.default
+           content.title = "AndroidLikeAlerm"
+           content.body = args["content"]!
+           content.sound = UNNotificationSound.default
 
             let request = UNNotificationRequest(identifier: "alarm", content: content, trigger: trigger)
 
