a486bdd46993058a7f549eefbfde6ec56ee976a0
quotto <5211139+quotto@users.noreply.github.com>
2022-11-04 01:16:06 +0900

    [test]ネイティブ（iOS）でのアラーム試行

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -16,32 +16,32 @@ import workmanager
     GeneratedPluginRegistrant.register(with: self)
 
     let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
-    let alarmChannel = FlutterMethodChannel(name: "net.mythrowtrash/alarm", binaryMessage: controller as! FlutterBinaryMessage)
+    let alarmChannel = FlutterMethodChannel(name: "net.mythrowtrash/alarm", binaryMessenger: controller as! FlutterBinaryMessenger)
     alarmChannel.setMethodCallHandler({
         (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
         switch call.method {
         case "reserveNextAlarm":
-            guard let args = call.arguments as? [String: NSNumber] else {return}
+        guard let args = call.arguments as? [String: NSNumber] else {return}
 
-            let now = Date()
-            var nextDate = now.addingTimeInterval(args["duration"]!.toInt())
-            var dateComponentsDay = Calendar.dateComponents(in: TimeZone.current, from:nextDate)
+        let now = Date()
+        var nextDate = now.addingTimeInterval(args["duration"]!.toInt())
+        var dateComponentsDay = Calendar.dateComponents(in: TimeZone.current, from:nextDate)
 
-            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponentsDay, repeats: false)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponentsDay, repeats: false)
 
-            let content = UNMutableNotificationContent()
-           content.title = "AndroidLikeAlerm"
-           content.body = args["content"]!
-           content.sound = UNNotificationSound.default
+        let content = UNMutableNotificationContent()
+        content.title = "AndroidLikeAlerm"
+        content.body = args["content"]!
+        content.sound = UNNotificationSound.default
 
-            let request = UNNotificationRequest(identifier: "alarm", content: content, trigger: trigger)
+        let request = UNNotificationRequest(identifier: "alarm", content: content, trigger: trigger)
 
-           notificationCenter.add(request) { (error) in
-               if error != nil {
-                   print(error.debugDescription)
-               }
+        notificationCenter.add(request) { (error) in
+           if error != nil {
+               print(error.debugDescription)
            }
-        }
+       }
+    }
     })
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
