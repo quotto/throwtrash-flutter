728e3faacf397caf348d7ec4c05888f6ea7a16f3
quotto <5211139+quotto@users.noreply.github.com>
2022-11-07 21:11:42 +0900

    [test]ネイティブ（iOS）でのアラーム試行

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -21,30 +21,34 @@ import workmanager
         (call: FlutterMethodCall, result: @escaping FlutterResult) -> Void in
         switch call.method {
         case "reserveNextAlarm":
-        guard let args = call.arguments as? [String: String] else {return}
+            guard let args = call.arguments as? [String: String] else {return}
 
-        let now = Date()
-        var nextDate = now.addingTimeInterval(Double(args["duration"]!)!)
-        let cal = Calendar(identifier: .gregorian)
-        var dateComponentsDay = cal.dateComponents(in: TimeZone.current, from:nextDate)
+            let now = Date()
+            var nextDate = now.addingTimeInterval(Double(args["duration"]!)!)
+            let cal = Calendar(identifier: .gregorian)
+            var dateComponentsDay = cal.dateComponents(in: TimeZone.current, from:nextDate)
 
-        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponentsDay, repeats: false)
+            let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponentsDay, repeats: false)
 
-        let content = UNMutableNotificationContent()
-        content.title = "AndroidLikeAlerm"
-        content.body = args["content"]!
-        content.sound = UNNotificationSound.default
+            let content = UNMutableNotificationContent()
+            content.title = "AndroidLikeAlerm"
+            content.body = args["content"]!
+            content.sound = UNNotificationSound.default
 
-        let request = UNNotificationRequest(identifier: "alarm", content: content, trigger: trigger)
+            let request = UNNotificationRequest(identifier: "alarm", content: content, trigger: trigger)
 
-        let notificationCenter = UNUserNotificationCenter.current()
+            let notificationCenter = UNUserNotificationCenter.current()
 
-        notificationCenter.add(request) { (error) in
-           if error != nil {
-               print(error.debugDescription)
-           }
+            notificationCenter.add(request) { (error) in
+               if error != nil {
+                   print(error.debugDescription)
+               }
+            }
+            result("alarm set")
+        default:
+            print("method channel not match")
+            result(FlutterMethodNotImplemented)
         }
-    }
     })
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
